6eddb2803014e60f0a04c0fdb3d26552e71f04c8
pfurst <peter.furst.dev@gmail.com>
2023-07-26 10:44:51 +0200

    Open the .dat file and printing user choice

--- lotto-data/main.py
+++ lotto-data/main.py
@@ -7,10 +7,25 @@ fiftytwo_numbers = []
 for i in range(5):
     number_into_fiftytwo = int(input(f"Enter {i + 1} number: "))
     fiftytwo_numbers.append(number_into_fiftytwo)
-    print(type(number_into_fiftytwo))
-
-print(fiftytwo_numbers)
 
 # 2, sorting numbers
 fiftytwo_numbers = sorted(fiftytwo_numbers)
 print(fiftytwo_numbers)
+
+# 3, Choosing number
+user_number = int(input("Choose a number 1-52: "))
+print(user_number)
+
+# 4, Open the .dat file and printing user choice
+
+all_lotto_numbers = []
+with open("lottosz.dat", "r") as lotto:
+    for line in lotto:
+        act_numbers = []
+        act_line = line.split()
+        for item in act_line:
+            number_from_line = int(item)
+            act_numbers.append(number_from_line)
+        all_lotto_numbers.append(act_numbers)
+print(all_lotto_numbers[user_number - 1])
+
