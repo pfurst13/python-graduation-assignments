a8a65190e6d0e8bc2ecffd07a5ee02e5603db4fe
pfurst <peter.furst.dev@gmail.com>
2023-07-26 09:22:36 +0200

    Number uploads

--- lotto-data/main.py
+++ lotto-data/main.py
@@ -1,1 +1,11 @@
 """52nd week lotto numbers are as follows:  89 24 34 11 64"""
+
+# 1, user enters the numbers
+
+fiftytwo_numbers = []
+
+for i in range(5):
+    number_in_fiftytwo = input(f"Enter {i + 1} number: ")
+    fiftytwo_numbers.append(number_in_fiftytwo)
+
+print(fiftytwo_numbers)
