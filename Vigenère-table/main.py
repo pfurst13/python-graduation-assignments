5ef5b0af8cff6f8ff52ffac4df051cf7d128b218
pfurst <peter.furst.dev@gmail.com>
2023-08-01 13:48:21 +0200

    keyword ready

--- Vigenère-table/main.py
+++ Vigenère-table/main.py
@@ -6,13 +6,23 @@ def format_string(user_input):
 
 # 1, ask user to get 255 charachter input
 
-user_string = input("Enter a text max 255 characters: ")
+user_string = input("Enter a text max 255 characters no numbers: ")
 
-while len(user_string) > 255:
-    user_string = input("Too long. It can be 255 characters. Enter a new one: ")
+while len(user_string) > 255 or user_string.isalpha() == False:
+    user_string = input("Too long or contains numbers. It can be 255 characters. Enter a new one: ")
 
-# 2, format string to english
+# 2, 3 format string to english print out
 user_string = format_string(user_string)
 print(user_string)
 
+# 4, get keyword from user
 
+user_key_word = input("Enter a keyword length has to be 5 characters and no numbers: ")
+keyword_len = len(user_key_word)
+print(keyword_len)
+
+while keyword_len != 5 or user_key_word.isalpha() == False:
+    user_key_word = input("Keyword has to be 5 characters and no numbers: ")
+    keyword_len = len(user_key_word)
+
+user_key_word = format_string(user_key_word)
