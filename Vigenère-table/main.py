c5d196d44e5c8d05fc76e6eed523a8e61606008c
pfurst <peter.furst.dev@gmail.com>
2023-08-04 10:56:28 +0200

    eyword concatenation

--- Vigenère-table/main.py
+++ Vigenère-table/main.py
@@ -8,27 +8,36 @@ def format_string(user_input):
 # 1, ask user to get 255 charachter input
 
 user_string = input("Enter a text max 255 characters no numbers: ")
-user_string = user_string.strip("")
+user_string = format_string(user_string)
+print(user_string)
+user_string = user_string.replace(' ', '')
+
 while len(user_string) > 255 or user_string.isalpha() == False:
+    print(user_string.isalpha())
     user_string = input("Too long or contains numbers. It can be 255 characters. Enter a new one: ")
-    user_string = user_string.strip("")
+    user_string = format_string(user_string)
+    user_string = user_string.replace(' ', '')
 
 # 2, 3 format string to english print out
-user_string = format_string(user_string)
 print(user_string)
 
 # 4, get keyword from user
 
-user_key_word = input("Enter a keyword length has to be 5 characters and no numbers: ")
+user_key_word = input("Enter a keyword length has to be maximum 5 characters and no numbers: ")
 keyword_len = len(user_key_word)
 print(keyword_len)
 
-while keyword_len != 5 or user_key_word.isalpha() == False:
-    user_key_word = input("Keyword has to be 5 characters and no numbers: ")
+while keyword_len > 5 or user_key_word.isalpha() == False:
+    user_key_word = input("Keyword has to be maximum 5 characters and no numbers: ")
     keyword_len = len(user_key_word)
 
 user_key_word = format_string(user_key_word)
 
 # 5, keyword concatenation
+concanate_user_key = ""
+for i in range(len(user_string)):
+    concanate_user_key += user_key_word
 
 
+concanate_user_key = concanate_user_key[:len(user_string)]
+print(user_string, len(user_string), concanate_user_key, len(concanate_user_key))
