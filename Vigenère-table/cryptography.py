e67c63a874a9e0d0341142c88d0d29643ccb4fd9
pfurst <peter.furst.dev@gmail.com>
2023-08-08 12:58:05 +0200

    Class Crypto coded_text need test

--- Vigenère-table/cryptography.py
+++ Vigenère-table/cryptography.py
@@ -1,4 +1,17 @@
 class Crypto:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, concanate_keyword, coding_table):
+        self.concanate_keyword = concanate_keyword
+        self.coding_table = coding_table
+
+    def coded_text(self, formated_user_text):
+        final_result = ""
+        for i in range(len(formated_user_text)):
+            column = 0
+            row = 0
+            for j, line in enumerate(self.coding_table):
+                if line[0] == formated_user_text[i]:
+                    column = j
+                    row = self.coding_table[0].index(self.concanate_keyword[i])
+                final_result.join(self.coding_table[column][row])
+        return final_result
