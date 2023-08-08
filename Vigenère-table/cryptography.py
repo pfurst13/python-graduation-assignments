f568f2eaf47ac2bdc21dc853219c23ff77600406
pfurst <peter.furst.dev@gmail.com>
2023-08-08 14:19:04 +0200

    concanating  not ready in class gonna be a new def

--- Vigenère-table/cryptography.py
+++ Vigenère-table/cryptography.py
@@ -1,10 +1,17 @@
 class Crypto:
 
-    def __init__(self, concanate_keyword, coding_table):
-        self.concanate_keyword = concanate_keyword
+    def __init__(self, keyword, coding_table):
+        self.keyword = keyword
         self.coding_table = coding_table
 
     def coded_text(self, formated_user_text):
+
+        concanate_keyword = ""
+        for i in range(len(formated_user_text)):
+            concanate_keyword += formated_user_text
+
+        concanate_keyword = concanate_keyword[:len(formated_user_text)]
+
         final_list = []
         for i in range(len(formated_user_text)):
             column = 0
@@ -12,7 +19,7 @@ class Crypto:
             for j, line in enumerate(self.coding_table):
                 if line[0] == formated_user_text[i]:
                     column = j
-            row = self.coding_table[0].index(self.concanate_keyword[i])
+            row = self.coding_table[0].index(concanate_keyword[i])
             final_list.append(self.coding_table[column][row])
         
         final_result = "".join(final_list)
