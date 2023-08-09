455ba0c47b031577f3f82baa3934c43b5a263efa
pfurst <peter.furst.dev@gmail.com>
2023-08-09 12:07:54 +0200

    string_format.py not ready

---
+++ Vigenère-table/string_format.py
@@ -0,0 +1,20 @@
+from unidecode import unidecode
+import string
+
+def format_from_keyboard(user_input):
+    user_input = unidecode(user_input)
+    user_input = user_input.translate(str.maketrans('','', string.punctuation))
+
+    return user_input.upper()
+
+def format_from_file(txt_input):
+
+    to_format_file = ""
+
+    with open(txt_input, "r") as text:
+        to_format_file = text.read()
+
+    to_format_file = unidecode(to_format_file)
+    to_format_file = to_format_file.translate(str.maketrans('','', string.punctuation))
+    
+    return to_format_file
