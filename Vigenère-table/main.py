91aeb797240e1d3de2705ed6da7f0872e8026d14
pfurst <peter.furst.dev@gmail.com>
2023-08-14 11:38:54 +0200

    input coding and decoding is working

--- Vigenère-table/main.py
+++ Vigenère-table/main.py
@@ -2,41 +2,26 @@ from unidecode import unidecode
 import string
 import cryptography
 import tables
+import string_format
 
 
 # 1 user input
 
-user_string = input("Enter a text max 255 characters no numbers: ")
-user_string = format_string(user_string)
-print(user_string)
-user_string = user_string.replace(' ', '')
-
-while len(user_string) > 255 or user_string.isalpha() == False:
-    print(user_string.isalpha())
-    user_string = input("Too long or contains numbers. It can be 255 characters. Enter a new one: ")
-    user_string = format_string(user_string)
-    user_string = user_string.replace(' ', '')
-
-# 2, 3 format string to english print out
-print(user_string)
+user_input = input("Enter a text max 255 characters: ")
+user_input = string_format.format_from_keyboard(user_input)
+print(user_input)
 
 # 4, get keyword from user
 
 user_key_word = input("Enter a keyword length has to be maximum 5 characters and no numbers: ")
-keyword_len = len(user_key_word)
-print(keyword_len)
+user_key_word = string_format.format_from_keyboard(user_key_word)
+print(user_key_word)
 
-while keyword_len > 5 or user_key_word.isalpha() == False:
-    user_key_word = input("Keyword has to be maximum 5 characters and no numbers: ")
-    keyword_len = len(user_key_word)
-
-user_key_word = format_string(user_key_word)
-
-vtable_list = tables.read_tabels("/Users/furst_peter/python-graduation-assignments/python-graduation-assignments/Vigenère-table/Vtabla.dat")
+vtable_list = tables.read_tabels("/Users/furst_peter/python-graduation-assignments/python-graduation-assignments/Vigenère-table/table.txt")
 
 coding = cryptography.Crypto(user_key_word, vtable_list)
 
-result_text = coding.coded_text(user_string)
+result_text = coding.coded_text(user_input)
 
 print(result_text)
 print(coding.decode_text(result_text))
