8feb100c5f414a1cf89e14dba19e68a5abe797e4
pfurst <peter.furst.dev@gmail.com>
2023-08-14 14:22:13 +0200

    True / False using default or txt vtable

--- Vigenère-table/main.py
+++ Vigenère-table/main.py
@@ -5,15 +5,23 @@ import string_format
 
 # 1 user input
 
-user_input = input("Enter a text max 255 characters: ")
+user_input = input("Enter your text: ")
 user_input = string_format.format_from_keyboard(user_input)
 
 # 4, get keyword from user
 
-user_key_word = input("Enter a keyword length has to be maximum 5 characters and no numbers: ")
+user_key_word = input("Enter your keyword: ")
 user_key_word = string_format.format_from_keyboard(user_key_word)
-# vtable_source = input("Add the vtable locaton route: ")
-vtable_list = tables.default_table()
+
+is_default = True
+
+
+
+if is_default == False: 
+    vtable_source = input("Add the vtable locaton route: ")
+    vtable_list = tables.read_tabels(vtable_source)
+elif is_default:
+    vtable_list = tables.default_table()
 
 coding = cryptography.Crypto(user_key_word, vtable_list)
 
