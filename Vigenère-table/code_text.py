af69b585f9201d0e962d8b3b14b6f9d5c87fe4d1
pfurst <peter.furst.dev@gmail.com>
2023-08-17 13:40:57 +0200

    code_text.py ready to test

--- Vigenère-table/code_text.py
+++ Vigenère-table/code_text.py
@@ -3,5 +3,24 @@ import tables
 import string_format
 
 def coding_text():
-    user_input = input("Enter your text or upload the file: ")
-    user_input = string_format.format_from_keyboard(user_input)
+    user_input = input("Enter your text or press enter: ")
+
+    if user_input == "":
+        user_input = input("Type the file path: ")
+        user_input = string_format.format_from_file(user_input)
+    else: 
+        user_input = string_format.format_from_keyboard(user_input)
+
+    keyword = input("Give me the keyword: ")
+    coding_table = input("Upload your own table. If you would like to use defaoult table press enter: ")
+
+    if coding_table == "":
+        coding_table = tables.default_table()
+        crypting = cryptography.Crypto(keyword, coding_table)
+        crypting_text = crypting.coded_text(user_input)
+    else:
+        own_table = tables.read_tabels(coding_table)
+        crypting = cryptography.Crypto(keyword, own_table)
+        crypting_text = crypting.coded_text(user_input)
+
+    return crypting_text
--- Vigenère-table/main.py
+++ Vigenère-table/main.py
@@ -1,33 +1,12 @@
-import cryptography
-import tables
-import string_format
 import decode_text
+import code_text
 
 
+choose_coding = input("If you'd like to code the text use C, to decode use D: ")
+final_text = ""
+if choose_coding == "C":
+    final_text = code_text.coding_text()
+if choose_coding == "D":
+    final_text = decode_text.translate_code()
 
-# 1 user input
-
-user_input = input("Enter your text: ")
-user_input = string_format.format_from_keyboard(user_input)
-
-# 4, get keyword from user
-
-user_key_word = input("Enter your keyword: ")
-user_key_word = string_format.format_from_keyboard(user_key_word)
-
-is_default = True
-
-if is_default == False: 
-    vtable_source = input("Add the vtable locaton route: ")
-    vtable_list = tables.read_tabels(vtable_source)
-elif is_default:
-    vtable_list = tables.default_table()
-
-coding = cryptography.Crypto(user_key_word, vtable_list)
-
-result_text = coding.coded_text(user_input)
-
-print(result_text)
-
-print(decode_text.translate_code())
-
+print(final_text)
